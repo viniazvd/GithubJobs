475171088e08ec04e2c69f8377843f867361641e
viniazvd <viniazvd@gmail.com>
2017-05-20 03:53:35 -0300

    nada naoo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Client = require("./../lib/index");
 const testAuth = require("./../testAuth.json");
 
+const callback = ( err, res ) => {
+    console.log(err, res);
+};
+
 const github = new Client({
     debug: true
 });
@@ -9,9 +13,5 @@ github.authenticate({
     type: "oauth",
     token: testAuth["token"]
 });
-
-const callback = ( err, res ) => {
-    console.log(err, res);
-};
 
 github.users.get({}, callback);
