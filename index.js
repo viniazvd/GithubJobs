df5a25f560c602b82a39b151ae9064d0e22b34fc
viniazvd <viniazvd@gmail.com>
2017-05-20 03:43:13 -0300

    getUsers

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-var GitHubApi = require("github");
- 
-var github = new GitHubApi({
-    // optional 
-    debug: true,
-    protocol: "https",
-    host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub 
-    pathPrefix: "/api/v3", // for some GHEs; none for GitHub 
-    headers: {
-        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent 
-    },
-    Promise: require('bluebird'),
-    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects 
-    timeout: 5000
+const Client = require("./../lib/index");
+const testAuth = require("./../testAuth.json");
+
+const github = new Client({
+    debug: true
 });
- 
-// TODO: optional authentication here depending on desired endpoints. See below in README. 
- 
-github.users.getFollowingForUser({
-    // optional 
-    // headers: { 
-    //     "cookie": "blahblah" 
-    // }, 
-    username: "defunkt"
-}, function(err, res) {
-    console.log(JSON.stringify(res));
+
+github.authenticate({
+    type: "oauth",
+    token: testAuth["token"]
 });
+
+const callback = ( err, res ) => {
+    console.log(err, res);
+};
+
+github.users.get({}, callback);
