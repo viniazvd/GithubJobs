e25d4363bfc5df9fd898bb06c4c2901da3a8700a
viniazvd <viniazvd@gmail.com>
2017-05-20 04:04:25 -0300

    foi mané

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 const Client = require( 'github' )
 const testAuth = require( './testAuth.json' )
 
-<<<<<<< HEAD
-const callback = ( err, res ) => {
-    console.log(err, res);
-};
-
-const github = new Client({
-    debug: true
-});
-=======
 const type = 'oauth'
 const token = testAuth[ 'token' ]
 const debug = true
->>>>>>> 7c640f13f8b49e361da4c050295e8f171fda5903
 
 const github = new Client( { debug } )
 
-<<<<<<< HEAD
-github.users.get({}, callback);
-=======
 const response = ( err, res ) => {
   console.log( 'err', err )
   console.log( 'res', res )
@@ -27,5 +14,10 @@ const response = ( err, res ) => {
 
 github.authenticate( { type, token } )
 
-github.users.get( {}, response )
->>>>>>> 7c640f13f8b49e361da4c050295e8f171fda5903
+//github.users.get( {}, response )
+
+github.gitdata.getReference({
+    owner: "kaizensoze",
+    repo: "test2",
+    ref: "heads/a#blah"
+}, response);
