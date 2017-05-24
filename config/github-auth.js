1b3c4169cf537c77e3f5e1b4214adc2b2f64fd59
viniazvd <viniazvd@gmail.com>
2017-05-24 18:00:24 -0300

    create gitignore

--- config/github-auth.js
+++ config/github-auth.js
@@ -1,5 +1,6 @@
 const Client = require( 'github' )
 const testAuth = require( './../testAuth.json' )
+
 const type = 'oauth'
 const token = testAuth[ 'token' ]
 const debug = true
--- controllers/git-controller.js
+++ controllers/git-controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const github = require('./../config/github-auth.js')
 const callbacks = require('./../services/callbacks')
 
